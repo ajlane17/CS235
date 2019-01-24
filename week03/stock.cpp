23836231e08741eed3e7e2e59641285a66a21ea8
Ethan Holden <isangakko@gmail.com>
2019-01-23 21:59:23 -0700

    Added the while loop for the prompt. Added stub functions to stock.cpp. Also added a Stock class for storing stocks within the queue.

--- week03/stock.cpp
+++ week03/stock.cpp
@@ -5,15 +5,66 @@
  *    This will contain the implementation for stocksBuySell() as well
  *    as any other function or class implementation you need
  * Author
- *    <your names here>
+ *    Adrian Lane, Ethan Holden
  **********************************************************************/
 
 #include <iostream>    // for ISTREAM, OSTREAM, CIN, and COUT
 #include <string>      // for STRING
+#include <sstream>     // for STRING STREAM
+#include <vector>      // for VECTOR
 #include <cassert>     // for ASSERT
 #include "stock.h"     // for STOCK_TRANSACTION
 #include "queue.h"     // for QUEUE
+#include "dollars.h"   // for DOLLARS
 using namespace std;
+
+class Stock
+{
+private:
+   int quantity;
+   Dollars cost;
+public:
+   // constructors
+   Stock() : quantity(0), cost(0) {}
+   Stock(const int & quantity, const Dollars & cost)
+   { this->quantity = quantity; this->cost = cost; }
+
+   // getters and setters
+   int getQuantity() { return quantity; }
+   Dollars getCost() { return cost; }
+
+   void setQuantity(int quantity)
+   { this->quantity = quantity; }
+   void setCost(Dollars cost)
+   { this->cost = cost; }
+   
+   //operators
+   Stock operator - (const int & rhs) const
+   {
+      return Stock(quantity - rhs, cost);
+   }
+   Stock operator + (const int & rhs) const
+   {
+      return Stock(quantity + rhs, cost);
+   }
+};
+
+void buyStocks(const int & q, const Dollars & p,
+               custom::queue <Stock> & portfolio)
+{
+   
+}
+
+void sellStocks(const int & q, const Dollars & p,
+                custom::queue <Stock> & portfolio)
+{
+
+}
+
+void display(custom::queue <Stock> & portfolio)
+{
+   
+}
 
 /************************************************
  * STOCKS BUY SELL
@@ -31,6 +82,36 @@ void stocksBuySell()
    cout << "  quit            - Display a final report and quit the program\n";
 
    // your code here...
+   string command;
+   int quantity;
+   Dollars price;
+   string input;
+   custom::queue <Stock> portfolio;
+
+   while (input != "quit")
+   {
+      // get input
+      cout << "> ";
+      cin.ignore();
+      getline(cin, input);
+
+      // parse input
+      stringstream ss(input);
+      ss >> command;
+      ss >> quantity;
+      ss >> price;
+
+      if (command == "buy")
+      {
+         buyStocks(quantity, price, portfolio);
+      }
+      else if (command == "sell")
+      {
+         sellStocks(quantity, price, portfolio);
+      }
+      else if (command == "display")
+      {
+         display(portfolio);
+      }
+   }
 }
-
-
