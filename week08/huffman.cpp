68d1d8fad35cf62a14e92d4c7114695fb9eb1ba2
Ethan Holden <isangakko@gmail.com>
2019-03-02 12:24:26 -0700

    Starting to work on huffman code

--- week08/huffman.cpp
+++ week08/huffman.cpp
@@ -3,7 +3,7 @@
  *    Week 08, Huffman
  *    Brother Helfrich, CS 235
  * Author:
- *    <your name>
+ *    Ethan Holden, Adrian Lane, Kenyon Bunker
  * Summary:
  *    This program will implement the huffman() function
  ************************************************************************/
@@ -13,7 +13,7 @@
 #include <cassert>         // for ASSERT
 #include <string>          // for STRING: binary representation of codes
 #include "bnode.h"         // for BINARY_NODE class definition
-//#include "vector.h"        // for VECTOR container
+#include "vector.h"        // for VECTOR container
 #include "pair.h"          // for PAIR container
 #include "huffman.h"       // for HUFFMAN() prototype
 
@@ -31,5 +31,66 @@ using namespace custom;
  *******************************************/
 void huffman(const string & fileName)
 {
+   vector <HuffTree> data = readFile(fileName);
+
+   //for error checking
+   for (int i = 0; i < data.size(); i++)
+      cout << data[i].root.data << endl;
+   
+
+   
+      
    return;
+}
+
+/*******************************************
+ * READ FILE
+ * Reads the file data into a vector of pairs
+ *******************************************/
+vector <HuffTree> readFile(const string & fileName)
+{
+   vector <pair <string, float>> data;
+   vector <HuffTree> trees;
+   
+   ifstream fin(fileName);
+   if (fin.fail())
+      return trees;
+
+   while (fin)
+   {
+      pair <string, float> temp;
+      fin >> temp;
+      if (temp.second != 0)
+      {
+          data.push_back(temp);
+      }
+   }
+
+   for (int i = 0; i < data.size(); i++)
+      trees.push_back(HuffTree(data[i]));
+   
+   return trees;
+}
+
+/*************************************************
+ * HUFF TREE
+ * NON-DEFAULT CONSTRUCTOR
+ * INPUT: BNODE
+ * Copies a BNode into the root of the HuffTree
+ *************************************************/
+HuffTree :: HuffTree (BNode <pair <string, float>> rhs)
+{
+   root = rhs;
+}
+
+/*************************************************
+ * HUFF TREE
+ * NON-DEFAULT CONSTRUCTOR
+ * INPUT: TWO HUFFTREE OBJECTS
+ * Copies two HuffTrees into a single one and adds
+ * their values together to make a root BNode.
+ *************************************************/
+HuffTree :: HuffTree (const HuffTree lhs, const HuffTree rhs)
+{
+      
 }
--- week08/huffman.h
+++ week08/huffman.h
@@ -11,7 +11,37 @@
 #ifndef HUFFMAN_H
 #define HUFFMAN_H
 
-#include <string>
-void huffman(const std::string & fileName);
+#include <iostream>        // for CIN and COUT
+#include <cassert>         // for ASSERT
+#include <string>          // for STRING: binary representation of codes
+#include "bnode.h"         // for BINARY_NODE class definition
+#include "vector.h"        // for VECTOR container
+#include "pair.h"          // for PAIR container
+
+using std::cout;
+using std::cin;
+using std::ifstream;
+using std::endl;
+using std::string;
+using std::bad_alloc;
+using namespace custom;
+
+class HuffTree
+{
+  public:
+   HuffTree() {}
+   HuffTree(BNode <pair <string, float>> rhs);
+   HuffTree(const HuffTree lhs, const HuffTree rhs);
+   
+   float getFrequency() { return root.data.second; }
+   float setFrequency(float num) { root.data.second = num; }
+      
+   //private:
+   BNode <pair <string, float>> root;
+ 
+};
+
+void huffman(const string & fileName);
+vector <HuffTree> readFile(const string & fileName);
 
 #endif // HUFFMAN_h
