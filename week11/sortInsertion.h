8a5501f0328467cb81a04802e24e0ad8e37a44e1
Adrian Lane <ajlane17@yahoo.com>
2019-03-22 13:43:05 -0600

    Implemented sortInsertion

--- week11/sortInsertion.h
+++ week11/sortInsertion.h
@@ -14,12 +14,61 @@
 #include <cassert>
 
 /*****************************************************
+ * BINARY SEARCH
+ * Returns insert location for item into sorted array
+ ****************************************************/
+template <class T>
+int binarySearch(T array[], T search, int iBegin, int iEnd)
+{
+   int iMiddle = (iBegin + iEnd) / 2;
+   if (iBegin > iEnd)
+   {
+      return iBegin;
+   }
+
+   if (array[iMiddle] == search)
+   {
+      return iMiddle;
+   }
+
+   if (search > array [iMiddle])
+   {
+      return binarySearch(array, search, iMiddle + 1, iEnd);
+   }
+
+   if (search < array[iMiddle])
+   {
+      return binarySearch(array, search, iBegin, iMiddle - 1);
+   }
+}
+
+/*****************************************************
  * SORT INSERTION
  * Perform the insertion sort
  ****************************************************/
 template <class T>
 void sortInsertion(T array[], int num)
 {
+   int iPivot;
+   int iInsert;
+   int iShift;
+   T valuePivot;
+
+   for (iPivot = num -2; iPivot >= 0; iPivot--)
+   {
+      valuePivot = array[iPivot];
+      
+      iInsert = binarySearch(array, valuePivot, iPivot + 1, num - 1);
+      
+      iInsert--;
+      
+      for (iShift = iPivot; iShift < iInsert; iShift++)
+      {
+         array[iShift] = array[iShift + 1];
+      }
+
+   array[iShift] = valuePivot;
+   }
 }
 
 
