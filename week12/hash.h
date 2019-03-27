61517cef48d0f819cea3086b371e526899271ed0
Ethan Holden <isangakko@gmail.com>
2019-03-26 18:54:30 -0600

    Added week12 folder and stub files.

---
+++ week12/hash.h
@@ -0,0 +1,23 @@
+/***********************************************************************
+ * Module:
+ *    Assignment 12, Spell Check
+ *    Brother Kirby, CS 235
+ * Author:
+ *    Ethan Holden, Adrian Lane, Kenyon Bunker
+ * Summary:
+ *    This program will implement the hash class
+ ************************************************************************/
+
+#ifndef HASH_H
+#define HASH_H
+
+
+class hash
+{
+  public:
+   
+  private:
+   
+};
+
+#endif // HASH_H
